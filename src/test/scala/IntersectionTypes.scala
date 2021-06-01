2d9c3e6377255c288b3191585dac13b811ff97c1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-06-02 00:38:27 +0900

    Create IntersectionTypes

---
+++ src/test/scala/IntersectionTypes.scala
@@ -0,0 +1,11 @@
+trait A:
+  def children: List[Int]
+
+trait B:
+  def children: List[Int]
+
+val x: A & B = new C
+val y: List[Int & Int] = x.children
+
+class C extends A, B:
+  def children: List[Int & Int] = List(1, 2, 3)
