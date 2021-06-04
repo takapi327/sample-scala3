24c469a6276c6c4064cd697b949d5d6315d519bc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-06-04 23:43:24 +0900

    Change

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,5 +5,11 @@ import UnionTypes._
   println(msg)
   println(leafElem(Seq(1, 3, 4)))
   println(help(UserName("takapi")))
+  println(bar[Int](List(1, 2, 3)))
 
 def msg = "I was compiled by Scala 3. :)"
+
+def foo[A](xs: List[A]): List[A] = xs.reverse
+
+val bar: [A] => List[A] => List[A]
+  = [A] => (xs: List[A]) => foo[A](xs)
