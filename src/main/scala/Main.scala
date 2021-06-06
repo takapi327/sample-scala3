bf9033e284822e51cc37560f2acac9993c472bfa
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-06-07 00:01:35 +0900

    Create Enum

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import UnionTypes._
+import Enumerations._
 
 @main def Main: Unit =
   println("Hello world!")
@@ -6,6 +7,12 @@ import UnionTypes._
   println(leafElem(Seq(1, 3, 4)))
   println(help(UserName("takapi")))
   println(bar[Int](List(1, 2, 3)))
+  println(Color.Red.ordinal)
+  println(Color.Green.ordinal)
+  println(Color.Blue.ordinal)
+  println(Color.valueOf("Blue"))
+  println(Color.values)
+  println(Color.fromOrdinal(0))
 
 def msg = "I was compiled by Scala 3. :)"
 
@@ -13,3 +20,19 @@ def foo[A](xs: List[A]): List[A] = xs.reverse
 
 val bar: [A] => List[A] => List[A]
   = [A] => (xs: List[A]) => foo[A](xs)
+
+enum Expr[A]:
+  case Var(name: String)
+  case Apply[A, B](fun: Expr[B => A], arg: Expr[B]) extends Expr[A]
+
+/*
+def mapSubexpressions[A](e: Expr[A])(f: [B] => Expr[B] => Expr[B]): Expr[A] =
+  e match
+    case Apply(fun, arg) => Apply(f(fun), f(arg))
+    case Var(n)          => Var(n)
+
+val e0 = Apply(Var("f"), Var("a"))
+val e1 = mapSubexpressions(e0)(
+  [B] => (se: Expr[B]) => Apply(Var[B => B]("wrap"), se)
+)
+*/
