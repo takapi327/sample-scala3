46aeb9cc4504980fb89cc1c46be20fc0d002a16a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-04 19:10:33 +0900

    Create Cuntry

--- database/mysql_data/sample_doobie.sql
+++ database/mysql_data/sample_doobie.sql
@@ -1,3 +1,18 @@
 CREATE DATABASE `sample_doobie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE `sample_doobie`;
+
+DROP TABLE IF EXISTS `country`;
+
+CREATE TABLE country (
+  code       character(3)  NOT NULL,
+  name       text          NOT NULL,
+  population integer       NOT NULL,
+  gnp        numeric(10,2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `country` WRITE;
+INSERT INTO `country` VALUES ('1', 'Afghanistan', 1, 1.1),
+                             ('2', 'Netherlands', 2, 1.2),
+                             ('3', 'Netherlands Antilles', 3, 1.3),
+                             ('4', 'Albania', 4, 1.4);
