b0537e92f1f6243ccdae742458be927e8d537c8a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-04 19:10:41 +0900

    Create program4

--- modules/library-rds/src/main/scala/library/rds/DoobieDatabaseConfig.scala
+++ modules/library-rds/src/main/scala/library/rds/DoobieDatabaseConfig.scala
@@ -2,8 +2,10 @@ package library.rds
 
 import doobie.*
 import doobie.implicits.*
+import doobie.util.ExecutionContexts
 
 import cats.*
+import cats.data.*
 import cats.effect.*
 import cats.implicits.*
 
@@ -20,6 +22,14 @@ val xa = Transactor.fromDriverManager[IO](
 
 val program1 = 42.pure[ConnectionIO]
 val program2 = sql"select 42".query[Int].unique
+val program3: ConnectionIO[(Int, Double)] =
+  for
+    a <- sql"select 42".query[Int].unique
+    b <- sql"select 52".query[Int].unique
+  yield (a, b)
+val program4 = sql"select name from country".query[String].to[List]
 
 val io  = program1.transact(xa)
 val io2 = program2.transact(xa)
+val io3 = program3.transact(xa)
+val io4 = program4.transact(xa)
--- src/main/scala/DoobieMain.scala
+++ src/main/scala/DoobieMain.scala
@@ -7,3 +7,5 @@ import library.rds.*
 @main def DoobieMain: Unit =
   println(io.unsafeRunSync())
   println(io2.unsafeRunSync())
+  println(io3.unsafeRunSync())
+  io4.unsafeRunSync().take(5).foreach(println)
