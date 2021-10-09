3fbd179998086dda90b0135768f4516558b6f428
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-09 15:11:22 +0900

    Create Person Table

--- database/mysql_data/sample_doobie.sql
+++ database/mysql_data/sample_doobie.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE `sample_doobie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE sample_doobie DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE `sample_doobie`;
+USE sample_doobie;
 
-DROP TABLE IF EXISTS `country`;
+DROP TABLE IF EXISTS country;
 
 CREATE TABLE country (
   code       character(3)  NOT NULL,
@@ -11,8 +11,20 @@ CREATE TABLE country (
   gnp        numeric(10,2)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-LOCK TABLES `country` WRITE;
-INSERT INTO `country` VALUES ('USA', 'Afghanistan', 1, 1.1),
-                             ('BRA', 'Netherlands', 2, 1.2),
-                             ('PAK', 'Netherlands Antilles', 3, 1.3),
-                             ('GBR', 'Albania', 4, 1.4);
+LOCK TABLES country WRITE;
+INSERT INTO country VALUES ('USA', 'Afghanistan', 1, 1.1),
+                           ('BRA', 'Netherlands', 2, 1.2),
+                           ('PAK', 'Netherlands Antilles', 3, 1.3),
+                           ('GBR', 'Albania', 4, 1.4);
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS person;
+
+CREATE TABLE person (
+  id   SERIAL,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  age  SMALLINT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES person WRITE;
+UNLOCK TABLES;
