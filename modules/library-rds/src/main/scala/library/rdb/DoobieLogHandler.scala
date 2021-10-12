6c12dbc6d203853c3870660ef818a9a6b2ea3ae7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-12 23:12:53 +0900

    Fix Comment

--- modules/library-rds/src/main/scala/library/rdb/DoobieLogHandler.scala
+++ modules/library-rds/src/main/scala/library/rdb/DoobieLogHandler.scala
@@ -7,8 +7,7 @@ import library.util.TrackingLogging
 object DoobieLogHandler extends TrackingLogging:
   val trackingLogHandler: LogHandler =
     LogHandler {
-      case Success(sql, arguments, e1, e2) => // 成功時は何もしない、必要に応じて以下コメントアウトを解除する
-        /*
+      case Success(sql, arguments, e1, e2) =>
         trackingLogger.info(s"""Successful Statement Execution:
           |
           | ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
@@ -16,7 +15,6 @@ object DoobieLogHandler extends TrackingLogging:
           | arguments = [${arguments.mkString(", ")}]
           |   elapsed = ${e1.toMillis} ms exec + ${e2.toMillis} ms processing (${(e1 + e2).toMillis} ms total)
         """.stripMargin)
-         */
 
       case ProcessingFailure(sql, arguments, e1, e2, t) =>
         trackingLogger.error(s"""Failed Resultset Processing:
