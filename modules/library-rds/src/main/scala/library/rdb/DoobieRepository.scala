ef84fb3c94b4712b1483cfb5a6ea75646c286e95
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-17 17:51:47 +0900

    Fix DoobieRepository

--- modules/library-rds/src/main/scala/library/rdb/DoobieRepository.scala
+++ modules/library-rds/src/main/scala/library/rdb/DoobieRepository.scala
@@ -1,6 +1,8 @@
 package library.rdb
 
+import cats.effect.*
+
 import doobie.LogHandler
 
-trait DoobieRepository extends DoobieDatabaseConfig:
-  given logHandler: LogHandler = DoobieLogHandler.trackingLogHandler
+trait DoobieRepository[M[_]: Async] extends DoobieDatabaseConfig[M]:
+  given LogHandler = DoobieLogHandler.trackingLogHandler
