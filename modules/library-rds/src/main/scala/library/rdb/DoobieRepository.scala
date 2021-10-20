25b9ac7f06c195e76bf927cb90322306da4e6cd0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-20 23:25:59 +0900

    Create RepositpryCreator

--- modules/library-rds/src/main/scala/library/rdb/DoobieRepository.scala
+++ modules/library-rds/src/main/scala/library/rdb/DoobieRepository.scala
@@ -6,3 +6,11 @@ import doobie.LogHandler
 
 trait DoobieRepository[M[_]: Async] extends DoobieDatabaseConfig[M]:
   given LogHandler = DoobieLogHandler.trackingLogHandler
+
+import org.atnos.eff._
+
+sealed trait RepositoryOp[A]
+
+trait RepositpryCreator:
+
+  opaque type _repositoryOp[R] = RepositoryOp |= R
