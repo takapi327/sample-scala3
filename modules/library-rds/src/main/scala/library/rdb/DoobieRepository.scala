2e6885ac8604ed8bb89e6cb39ae4d1635f5dc270
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-22 23:36:53 +0900

    Delete unused

--- modules/library-rds/src/main/scala/library/rdb/DoobieRepository.scala
+++ modules/library-rds/src/main/scala/library/rdb/DoobieRepository.scala
@@ -8,9 +8,3 @@ trait DoobieRepository[M[_]: Async] extends DoobieDatabaseConfig[M]:
   given LogHandler = DoobieLogHandler.trackingLogHandler
 
 import org.atnos.eff._
-
-sealed trait RepositoryOp[A]
-
-trait RepositpryCreator:
-
-  opaque type _repositoryOp[R] = RepositoryOp |= R
