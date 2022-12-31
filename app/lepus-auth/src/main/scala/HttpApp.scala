fabb05187fdd78d134697ed823e61007d18d5538
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-12-31 16:21:55 +0900

    Fix HttpApp

--- app/lepus-auth/src/main/scala/HttpApp.scala
+++ app/lepus-auth/src/main/scala/HttpApp.scala
@@ -1,7 +1,5 @@
 package app
 
-import java.net.{ URI, URL }
-
 import cats.effect.IO
 
 import org.http4s.*
@@ -11,28 +9,29 @@ import org.http4s.headers.Location
 import org.http4s.implicits.*
 
 import lepus.app.LepusApp
-import lepus.logger.{LoggerF, LoggingIO, given}
+import lepus.logger.{ LoggerF, LoggingIO, given }
 
-import com.nimbusds.jose.JWSAlgorithm
-import com.nimbusds.jose.util.DefaultResourceRetriever
-import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator
-import com.nimbusds.oauth2.sdk.id.{ClientID, Issuer, State}
-import com.nimbusds.oauth2.sdk.auth.{ClientSecretBasic, Secret}
-import com.nimbusds.openid.connect.sdk.{ Nonce, Prompt, AuthenticationRequest, OIDCTokenResponse, OIDCTokenResponseParser }
+import com.nimbusds.openid.connect.sdk.{ Nonce, OIDCTokenResponse, OIDCTokenResponseParser }
+import com.nimbusds.oauth2.sdk.pkce.CodeVerifier
+
+import auth.*
 
 object HttpApp extends LepusApp[IO], LoggingIO:
+
+  val codeVerifier = new CodeVerifier()
+  val nonce = new Nonce()
 
   private val authRoutes = HttpRoutes.of[IO] {
     case GET -> Root / "login" =>
       //println("=============================")
       //println(AuthenticationProvider.loginURL)
       //println("=============================")
-      IO(Response(status = SeeOther, headers = Headers(Location(Uri.unsafeFromString(AuthenticationProvider.loginURL)))))
+      IO(Response(status = SeeOther, headers = Headers(Location(Uri.unsafeFromString(AuthenticationProvider.loginURL(nonce, codeVerifier))))))
     case GET -> Root / "logout" =>
       IO(Response(status = SeeOther, headers = Headers(Location(Uri.unsafeFromString(AuthenticationProvider.logoutURL)))))
     case req@GET -> Root / "callback" =>
       for
-        response <- IO(IdTokenProvider.request(using req).toHTTPRequest.send())
+        response <- IO(IdTokenProvider.request(codeVerifier)(using req).toHTTPRequest.send())
       yield
         //println(IdTokenProvider.request.toHTTPRequest.getURL.toString + "?" + IdTokenProvider.request.toHTTPRequest.getQuery)
         val oidc     = OIDCTokenResponseParser.parse(response)
@@ -43,13 +42,7 @@ object HttpApp extends LepusApp[IO], LoggingIO:
           println(s"Refresh Token: ${res.getOIDCTokens.getRefreshToken.getValue}")
           println("成功")
 
-          val issuer    = new Issuer("https://dev-nw1kyvpiev7up1bq.us.auth0.com/")
-          val clientID  = new ClientID(AuthenticationProvider.clientId)
-          val jwkSetURL = new URL("https://dev-nw1kyvpiev7up1bq.us.auth0.com/.well-known/jwks.json")
-
-          val validator = new IDTokenValidator(issuer, clientID, JWSAlgorithm.RS256, jwkSetURL, new DefaultResourceRetriever(2000, 2000))
-
-          val result = validator.validate(res.getOIDCTokens.getIDToken, nonce)
+          val result = IDTokenParser.validator.validate(res.getOIDCTokens.getIDToken, nonce)
           println(result.toJWTClaimsSet.getClaims)
           println(result.getStringClaim("name"))
           println(result.getStringClaim("email"))
@@ -74,62 +67,3 @@ object HttpApp extends LepusApp[IO], LoggingIO:
   override val errorHandler: PartialFunction[Throwable, IO[Response[IO]]] =
     case error: Throwable => logger.error(s"Unexpected error: $error", error)
       .as(Response(Status.InternalServerError))
-
-import lepus.core.util.Configuration
-
-import com.nimbusds.oauth2.sdk.{ ResponseType, Scope }
-import com.nimbusds.oauth2.sdk.pkce.{ CodeVerifier, CodeChallengeMethod }
-
-val codeVerifier = new CodeVerifier()
-val nonce        = new Nonce()
-
-object AuthenticationProvider:
-
-  private val config = Configuration.load()
-
-  private val domain   = config.get[String]("com.auth0.domain")
-  val clientId = config.get[String]("com.auth0.clientId")
-
-  private val authenticationEndpoint = config.get[String]("com.auth0.authentication.endpoint")
-  private val authenticationScopes   = config.get[Seq[String]]("com.auth0.authentication.scopes")
-
-  private val base: AuthenticationRequest.Builder = new AuthenticationRequest.Builder(
-    new ResponseType("code"),
-    authenticationScopes.foldLeft(new Scope("openid"))((acc, scope) => {
-      acc.add(scope)
-      acc
-    }),
-    new ClientID(clientId),
-    new URI("http://localhost:5555/callback")
-  ).endpointURI(new URI(authenticationEndpoint))
-    .state(new State())
-    .nonce(nonce)
-    .codeChallenge(codeVerifier, CodeChallengeMethod.S256)
-
-  val loginURL = base.build().toURI.toASCIIString
-  val logoutURL = s"https://$domain/v2/logout?client_id=$clientId&returnTo=http://localhost:5555/login"
-
-import com.nimbusds.oauth2.sdk.{ AuthorizationCode, AuthorizationCodeGrant, TokenRequest }
-import com.nimbusds.openid.connect.sdk.{ AuthenticationSuccessResponse, AuthenticationErrorResponse, AuthenticationResponseParser }
-
-object IdTokenProvider:
-
-  private val config = Configuration.load()
-
-  private val tokenEndpoint = config.get[String]("com.auth0.token.endpoint")
-  private val clientId      = config.get[String]("com.auth0.clientId")
-  private val clientSecret  = config.get[String]("com.auth0.clientSecret")
-
-  private def response(using request: Request[IO]): AuthenticationSuccessResponse | AuthenticationErrorResponse =
-    val respParser = AuthenticationResponseParser.parse(new URI("http://" + request.uri))
-    if respParser.indicatesSuccess() then respParser.toSuccessResponse else respParser.toErrorResponse
-
-  private val validateCallback: Request[IO] ?=> String = response match
-    case r: AuthenticationErrorResponse   => r.getErrorObject.getCode
-    case r: AuthenticationSuccessResponse => r.getAuthorizationCode.getValue
-
-  private val clientSecretBasic = new ClientSecretBasic(new ClientID(clientId), new Secret(clientSecret))
-  private val codeGrant: Request[IO] ?=> AuthorizationCodeGrant =
-    new AuthorizationCodeGrant(new AuthorizationCode(validateCallback), new URI("http://localhost:5555/callback"), new CodeVerifier(codeVerifier.getValue))
-
-  val request: Request[IO] ?=> TokenRequest = new TokenRequest(new URI(tokenEndpoint), clientSecretBasic, codeGrant)
