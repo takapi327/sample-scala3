71ac56feb10ca0eb1c24341588f1617988106846
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-12-31 15:56:36 +0900

    Fix IDTokenValidator

--- app/lepus-auth/src/main/scala/HttpApp.scala
+++ app/lepus-auth/src/main/scala/HttpApp.scala
@@ -1,6 +1,6 @@
 package app
 
-import java.net.URI
+import java.net.{ URI, URL }
 
 import cats.effect.IO
 
@@ -11,9 +11,14 @@ import org.http4s.headers.Location
 import org.http4s.implicits.*
 
 import lepus.app.LepusApp
-import lepus.logger.{ LoggerF, LoggingIO, given }
+import lepus.logger.{LoggerF, LoggingIO, given}
 
-import com.nimbusds.openid.connect.sdk.{ OIDCTokenResponse, OIDCTokenResponseParser }
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.util.DefaultResourceRetriever
+import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator
+import com.nimbusds.oauth2.sdk.id.{ClientID, Issuer, State}
+import com.nimbusds.oauth2.sdk.auth.{ClientSecretBasic, Secret}
+import com.nimbusds.openid.connect.sdk.{ Nonce, Prompt, AuthenticationRequest, OIDCTokenResponse, OIDCTokenResponseParser }
 
 object HttpApp extends LepusApp[IO], LoggingIO:
 
@@ -23,6 +28,8 @@ object HttpApp extends LepusApp[IO], LoggingIO:
       //println(AuthenticationProvider.loginURL)
       //println("=============================")
       IO(Response(status = SeeOther, headers = Headers(Location(Uri.unsafeFromString(AuthenticationProvider.loginURL)))))
+    case GET -> Root / "logout" =>
+      IO(Response(status = SeeOther, headers = Headers(Location(Uri.unsafeFromString(AuthenticationProvider.logoutURL)))))
     case req@GET -> Root / "callback" =>
       for
         response <- IO(IdTokenProvider.request(using req).toHTTPRequest.send())
@@ -35,6 +42,18 @@ object HttpApp extends LepusApp[IO], LoggingIO:
           println(s"Access Token:  ${res.getOIDCTokens.getAccessToken.getValue}")
           println(s"Refresh Token: ${res.getOIDCTokens.getRefreshToken.getValue}")
           println("成功")
+
+          val issuer    = new Issuer("https://dev-nw1kyvpiev7up1bq.us.auth0.com/")
+          val clientID  = new ClientID(AuthenticationProvider.clientId)
+          val jwkSetURL = new URL("https://dev-nw1kyvpiev7up1bq.us.auth0.com/.well-known/jwks.json")
+
+          val validator = new IDTokenValidator(issuer, clientID, JWSAlgorithm.RS256, jwkSetURL, new DefaultResourceRetriever(2000, 2000))
+
+          val result = validator.validate(res.getOIDCTokens.getIDToken, nonce)
+          println(result.toJWTClaimsSet.getClaims)
+          println(result.getStringClaim("name"))
+          println(result.getStringClaim("email"))
+          println(result.getStringClaim("nickname"))
         else
           val res = oidc.toErrorResponse
           println(s"Code:           ${res.getErrorObject.getCode}")
@@ -58,20 +77,18 @@ object HttpApp extends LepusApp[IO], LoggingIO:
 
 import lepus.core.util.Configuration
 
-import com.nimbusds.oauth2.sdk.id.{ ClientID, State }
 import com.nimbusds.oauth2.sdk.{ ResponseType, Scope }
-import com.nimbusds.openid.connect.sdk.{ Nonce, Prompt, AuthenticationRequest }
 import com.nimbusds.oauth2.sdk.pkce.{ CodeVerifier, CodeChallengeMethod }
 
 val codeVerifier = new CodeVerifier()
+val nonce        = new Nonce()
 
 object AuthenticationProvider:
 
   private val config = Configuration.load()
 
-  private val domain       = config.get[String]("com.auth0.domain")
-  private val clientId     = config.get[String]("com.auth0.clientId")
-  private val clientSecret = config.get[String]("com.auth0.clientSecret")
+  private val domain   = config.get[String]("com.auth0.domain")
+  val clientId = config.get[String]("com.auth0.clientId")
 
   private val authenticationEndpoint = config.get[String]("com.auth0.authentication.endpoint")
   private val authenticationScopes   = config.get[Seq[String]]("com.auth0.authentication.scopes")
@@ -86,13 +103,12 @@ object AuthenticationProvider:
     new URI("http://localhost:5555/callback")
   ).endpointURI(new URI(authenticationEndpoint))
     .state(new State())
-    .nonce(new Nonce())
+    .nonce(nonce)
     .codeChallenge(codeVerifier, CodeChallengeMethod.S256)
 
-  //val loginURL = uri"https://dev-nw1kyvpiev7up1bq.us.auth0.com/authorize?response_type=code&client_id=FjRbIrTPDqDcbCRpMKc6QdVggkZs3yzS&connection=Username-Password-Authentication&redirect_uri=http://localhost:5555/callback&state=STATE"
   val loginURL = base.build().toURI.toASCIIString
+  val logoutURL = s"https://$domain/v2/logout?client_id=$clientId&returnTo=http://localhost:5555/login"
 
-import com.nimbusds.oauth2.sdk.auth.{ ClientSecretBasic, Secret }
 import com.nimbusds.oauth2.sdk.{ AuthorizationCode, AuthorizationCodeGrant, TokenRequest }
 import com.nimbusds.openid.connect.sdk.{ AuthenticationSuccessResponse, AuthenticationErrorResponse, AuthenticationResponseParser }
 
