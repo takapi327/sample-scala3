d917cbd19efecf39eda30c587d3f2dd679d51762
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-01-02 15:35:57 +0900

    Fix HttpApp

--- app/lepus-auth/src/main/scala/HttpApp.scala
+++ app/lepus-auth/src/main/scala/HttpApp.scala
@@ -1,6 +1,6 @@
 package app
 
-import cats.effect.IO
+import cats.effect.{ IO, Resource }
 
 import org.http4s.*
 import org.http4s.dsl.io.*
@@ -34,7 +34,7 @@ object HttpApp extends LepusApp[IO], LoggingIO:
         response <- IO(IdTokenProvider.request(codeVerifier)(using req).toHTTPRequest.send())
       yield
         //println(IdTokenProvider.request.toHTTPRequest.getURL.toString + "?" + IdTokenProvider.request.toHTTPRequest.getQuery)
-        val oidc     = OIDCTokenResponseParser.parse(response)
+        val oidc = OIDCTokenResponseParser.parse(response)
         if oidc.indicatesSuccess() then
           val res = oidc.toSuccessResponse.asInstanceOf[OIDCTokenResponse]
           println(s"ID Token:      ${res.getOIDCTokens.getIDToken.serialize()}")
@@ -67,3 +67,71 @@ object HttpApp extends LepusApp[IO], LoggingIO:
   override val errorHandler: PartialFunction[Throwable, IO[Response[IO]]] =
     case error: Throwable => logger.error(s"Unexpected error: $error", error)
       .as(Response(Status.InternalServerError))
+
+import cats.syntax.all.*
+import cats.effect.*
+import org.http4s.ember.server.EmberServerBuilder
+import com.comcast.ip4s.*
+
+import org.typelevel.vault.{ Key, Vault }
+import com.google.inject.{ Guice, Injector, AbstractModule, TypeLiteral }
+
+import session.*
+
+object Example extends ResourceApp.Forever, LoggingIO:
+
+  val codeVerifier = new CodeVerifier()
+  val nonce = new Nonce()
+
+  private val userKey = SessionKey[User]
+
+  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "login" =>
+      SeeOther(Location(Uri.unsafeFromString(AuthenticationProvider.loginURL(nonce, codeVerifier))))
+    case GET -> Root / "logout" =>
+      SeeOther(Location(Uri.unsafeFromString(AuthenticationProvider.logoutURL)))
+        .map(_.withAttribute(VaultSessionMiddleware.VaultSessionReset.key, VaultSessionMiddleware.VaultSessionReset))
+    case req @ GET -> Root / "callback" => Controller.callback(req)
+    case req @ GET -> Root / "hello" => Controller.hello(req)
+  }
+
+  object Controller:
+
+    def callback(req: Request[IO]): IO[Response[IO]] =
+      for
+        response <- IO(IdTokenProvider.request(codeVerifier)(using req).toHTTPRequest.send())
+      yield
+        val oidc = OIDCTokenResponseParser.parse(response)
+        if oidc.indicatesSuccess() then
+          val res = oidc.toSuccessResponse.asInstanceOf[OIDCTokenResponse]
+          val result = IDTokenParser.validator.validate(res.getOIDCTokens.getIDToken, nonce)
+          Response(status = SeeOther, headers = Headers(Location(uri"hello")))
+            .withAttribute(userKey, User(result.getStringClaim("name"), result.getStringClaim("email")))
+        else
+          //val res = oidc.toErrorResponse
+          Response(Unauthorized)
+
+    def hello(req: Request[IO]): IO[Response[IO]] =
+      Ok(
+        req.attributes.lookup(userKey).fold("なし")(user => s"User name: ${user.name}, email: ${user.email}")
+      )
+
+  case class User(name: String, email: String)
+
+  override def run(args: List[String]): Resource[IO, Unit] =
+    for
+      storage <- Resource.eval(SessionStorage.create[IO, Vault]())
+      _ <- EmberServerBuilder.default[IO]
+             .withPort(port"5555")
+             .withHttpApp(
+               VaultSessionMiddleware.impl(storage, secure = false)(routes).orNotFound
+             )
+             .withErrorHandler({
+               case error: Throwable => logger.error(s"Unexpected error: $error", error)
+                 .as(Response(Status.InternalServerError))
+             })
+             .build
+    yield ()
+
+object SessionKey:
+  def apply[T]: Key[T] = Key.newKey[cats.effect.SyncIO, T].unsafeRunSync()
