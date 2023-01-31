1a117329d3bdd24e70f2dbdb7fbfa5a3ac18fd7d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-01-31 22:40:36 +0900

    Fix Table

--- modules/ldbc/src/main/scala/ldbc/Table.scala
+++ modules/ldbc/src/main/scala/ldbc/Table.scala
@@ -11,21 +11,38 @@ trait Table[F[_], T <: Product](name: String) extends example.ColumnAlias:
 
   def * : Seq[Column[F]]
 
-  protected final def column[A](_label: String, _type: Column.Type)(using _loader: ResultSetLoader[F, A]): Column[F] = new Column[F]:
+  protected final def column[A](
+    _label: String,
+    _type: Column.Type,
+    _options: ColumnOption.Key*
+  )(using _loader: ResultSetLoader[F, A]): Column[F] = new Column[F]:
     override type ThisType = A
 
     override def label: String = _label
 
     override def `type`: Column.Type = _type
 
+    override def options: Seq[ColumnOption.Key] = _options
+
     override def loader: ResultSetLoader[F, A] = _loader
+
+  def options: Seq[ColumnOption] = Seq.empty
 
   given schema: Schema
 
   def createTableQuery: String =
+    val primaryKey: Option[String] =
+      val columns = this.*.filter(_.options.contains(PrimaryKey))
+      if columns.nonEmpty then
+        Some(ColumnOption.PrimaryKey(columns.map(_.label): _*).toString)
+      else None
+
+    val settings: Seq[String] =
+      *.map(_.toString) ++ options.map(_.toString)
+
     s"""
        |CREATE TABLE `$name` (
-       |  ${*.mkString(",\n  ")}
+       |  ${settings.mkString(",\n  ")}
        |);
        |
        |""".stripMargin
