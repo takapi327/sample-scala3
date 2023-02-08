79abeb41efbaed55f450d4cd5712eb42388c1b76
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-08 23:39:03 +0900

    Fix Key

--- modules/ldbc/src/main/scala/ldbc/Key.scala
+++ modules/ldbc/src/main/scala/ldbc/Key.scala
@@ -1,8 +1,36 @@
 
 package ldbc
 
-enum Key:
-  case PRIMARY_KEY
-  case UNIQUE_KEY
-  case INDEX
-  case FOREIGN_KEY
+trait Key:
+
+  def queryString: String
+
+case class PrimaryKey[F[_]](column: Column[F]*) extends Key:
+
+  override def queryString: String = if column.nonEmpty then s"PRIMARY KEY(${column.mkString(",")})" else "PRIMARY KEY"
+
+  override def toString: String = "PRIMARY KEY"
+
+case class UniqueKey[F[_]](column: Column[F]*) extends Key:
+  override def queryString: String = if column.nonEmpty then s"UNIQUE KEY(${column.mkString(",")})" else "UNIQUE KEY"
+
+  override def toString: String = "UNIQUE KEY"
+
+case class Index() extends Key:
+  override def queryString: String = ""
+
+case class ForeignKey() extends Key:
+  override def queryString: String = ""
+
+case class AutoInc(key: Option[AutoInc.Key]):
+  def queryString: String = key.fold("AUTO_INCREMENT")(v => s"AUTO_INCREMENT $v")
+
+  override def toString: String = queryString
+
+object AutoInc:
+
+  enum Key(label: String):
+    case PRIMARY_KEY extends Key("PRIMARY KEY")
+    case UNIQUE_KEY  extends Key("UNIQUE KEY")
+
+    override def toString: String = label
