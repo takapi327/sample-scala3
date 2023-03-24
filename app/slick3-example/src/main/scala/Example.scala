c9632b0724e7a93baed537e9057497258c74200c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-03-24 22:48:51 +0900

    Fix Example

--- app/slick3-example/src/main/scala/Example.scala
+++ app/slick3-example/src/main/scala/Example.scala
@@ -1,23 +1,42 @@
 
+import java.time.LocalDateTime
+import com.mysql.cj.jdbc.MysqlDataSource
+
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
-import com.mysql.cj.jdbc.MysqlDataSource
+
 import slick.jdbc.GetResult
 import slick.jdbc.MySQLProfile.api.*
+import slick.lifted.ShapedValue
+import slick.sql.SqlProfile.ColumnOption.SqlType
 
-case class Person(
+case class User(
   id:   Option[Long],
   name: String,
   age:  Option[Int],
 )
 
-class PersonTable(tag: Tag) extends Table[Person](tag, "person"):
+class UserTable(tag: Tag) extends Table[User](tag, "user"):
   def id   = column[Long]("id", O.PrimaryKey, O.AutoInc)
-  def name = column[String]("name")
+  def name = column[String]("name", SqlType("BIGINT(64)"))
   def age  = column[Option[Int]]("age")
 
-  def * = (id.?, name, age).mapTo[Person]
+  def pk = primaryKey("pk_a", id)
+
+  def * = (id.?, name, age).mapTo[User]
+
+  println("======================")
+  println("======================")
+  println("======================")
+  val test: ShapedValue[Rep[String], String] = name
+  val test2: ShapedValue[(Rep[Option[Long]], Rep[String], Rep[Option[Int]]), (Option[Long], String, Option[Int])] =
+    (id.?, name, age)
+  println(test2)
+  println("======================")
+  println("======================")
+  println("======================")
+
 
 object Example:
 
@@ -28,18 +47,15 @@ object Example:
   dataSource.setUser("takapi327")
   dataSource.setPassword("takapi327")
 
-  private val personTable = TableQuery[PersonTable]
+  private val userTable = TableQuery[UserTable]
   private val db = Database.forDataSource(dataSource, None)
 
   def main(args: Array[String]): Unit =
-    //val query = for {
-    //  p <- personTable.filter(_.name === "takapi327")
-    //} yield p.name
-    given GetResult[Person] = GetResult(r => Person(r.<<?, r.<<, r.<<?))
-    val query = sql"SELECT * FROM person WHERE name = 'takapi327'".as[Person].headOption
-    val result = db.run(query)
+    //given GetResult[User] = GetResult(r => Person(r.<<?, r.<<, r.<<?))
+    //val query = sql"SELECT * FROM user WHERE name = 'takapi'".as[User].headOption
+    val result = db.run(userTable.filter(_.name === "takapi").result.headOption)
     result.map(println(_))
-    println(personTable.schema.create.statements)
+    println(userTable.schema.create.statements)
 
     Await.result(result, Duration.Inf)
     db.close
