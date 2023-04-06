60ef1a20ea5d095f64ee345b063c5f6d025aefe9
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-04-06 22:00:29 +0900

    Fix Example

--- app/slick3-example/src/main/scala/Example.scala
+++ app/slick3-example/src/main/scala/Example.scala
@@ -17,27 +17,6 @@ case class User(
   age:  Option[Int],
 )
 
-class UserTable(tag: Tag) extends Table[User](tag, "user"):
-  def id   = column[Long]("id", O.PrimaryKey, O.AutoInc)
-  def name = column[String]("name", SqlType("BIGINT(64)"))
-  def age  = column[Option[Int]]("age")
-
-  def pk = primaryKey("pk_a", id)
-
-  def * = (id.?, name, age).mapTo[User]
-
-  println("======================")
-  println("======================")
-  println("======================")
-  val test: ShapedValue[Rep[String], String] = name
-  val test2: ShapedValue[(Rep[Option[Long]], Rep[String], Rep[Option[Int]]), (Option[Long], String, Option[Int])] =
-    (id.?, name, age)
-  println(test2)
-  println("======================")
-  println("======================")
-  println("======================")
-
-
 object Example:
 
   private val dataSource = new MysqlDataSource()
@@ -47,15 +26,72 @@ object Example:
   dataSource.setUser("takapi327")
   dataSource.setPassword("takapi327")
 
-  private val userTable = TableQuery[UserTable]
+  class UserTable(tag: Tag) extends Table[User](tag, "user"):
+    def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+    def age = column[Option[Int]]("age")
+
+    def * = (id, name, age).mapTo[User]
+
+  private val userTable = TableQuery(new UserTable(_))
   private val db = Database.forDataSource(dataSource, None)
 
   def main(args: Array[String]): Unit =
-    //given GetResult[User] = GetResult(r => Person(r.<<?, r.<<, r.<<?))
-    //val query = sql"SELECT * FROM user WHERE name = 'takapi'".as[User].headOption
+
     val result = db.run(userTable.filter(_.name === "takapi").result.headOption)
     result.map(println(_))
-    println(userTable.schema.create.statements)
 
     Await.result(result, Duration.Inf)
     db.close
+
+
+//given GetResult[User] = GetResult(r => Person(r.<<?, r.<<, r.<<?))
+//val query = sql"SELECT * FROM user WHERE name = 'takapi'".as[User].headOption
+
+/*
+package ldbc.slick
+
+import com.mysql.cj.jdbc.MysqlDataSource
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import ldbc.core.syntax.given
+import ldbc.slick.jdbc.MySQLProfile.api.*
+
+
+object Example:
+
+  case class User(
+    id:   Option[Long],
+    name: String,
+    age:  Option[Int],
+  )
+
+  private val dataSource = new MysqlDataSource()
+  dataSource.setServerName("127.0.0.1")
+  dataSource.setPortNumber(13306)
+  dataSource.setDatabaseName("sample_doobie")
+  dataSource.setUser("takapi327")
+  dataSource.setPassword("takapi327")
+
+  private val table = Table[User]("user")(
+    column("id", BIGINT(64), PRIMARY_KEY, AUTO_INCREMENT),
+    column("name", VARCHAR(255)),
+    column("age", INT(255).DEFAULT_NULL),
+  )
+
+  private val userTable = SlickTableQuery(table)
+  private val db = Database.forDataSource(dataSource, None)
+
+  def main(args: Array[String]): Unit =
+
+    val result = db.run(userTable.filter(_.name === "takapi").result.headOption)
+    result.map(println(_))
+
+    Await.result(result, Duration.Inf)
+    db.close
+
+
+*/
