2b807afda7da13ebd195d3ac3e6f625487cc6a81
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-04-10 20:29:20 +0900

    Fix Main

--- app/ldbc-example/src/main/scala/Main.scala
+++ app/ldbc-example/src/main/scala/Main.scala
@@ -12,6 +12,20 @@ import ldbc.sql.*
 import ldbc.sql.syntax.{ *, given }
 import ldbc.dsl.io.*
 
+case class User(
+  id:        Long,
+  name:      String,
+  age:       Option[Int],
+  status:    User.Status,
+  updatedAt: LocalDateTime,
+  createdAt: LocalDateTime
+)
+
+object User:
+
+  enum Status(val code: Short):
+    case Active extends Status(1)
+
 object Main extends IOApp:
 
   private val dataSource = new MysqlDataSource()
@@ -21,35 +35,34 @@ object Main extends IOApp:
   dataSource.setUser("takapi327")
   dataSource.setPassword("takapi327")
 
-  case class User(
-    id:        Long,
-    name:      String,
-    age:       Option[Int],
-    updatedAt: LocalDateTime,
-    createdAt: LocalDateTime
-  )
+  given Conversion[SMALLINT[Short], DataType[User.Status]] = DataType.mapping[SMALLINT[Short], User.Status]
 
   private val table: Table[User] = Table[User]("user")(
     column("id", BIGINT(64), AUTO_INCREMENT, PRIMARY_KEY),
     column("name", VARCHAR(255)),
     column("age", INT(255).DEFAULT_NULL),
+    column("status", SMALLINT(255)),
     column("updated_at", TIMESTAMP.DEFAULT_CURRENT_TIMESTAMP()),
     column("created_at", TIMESTAMP.DEFAULT_CURRENT_TIMESTAMP(true))
   )
+
+  given ResultSetReader[IO, User.Status] =
+    ResultSetReader.mapping[IO, Short, User.Status](v => User.Status.values.find(_.code === v).get)
 
   given Kleisli[IO, ResultSet[IO], User] =
     for
       id        <- table.id()
       name      <- table.name()
       age       <- table.age()
+      status    <- table.status()
       updatedAt <- table.updatedAt()
       createdAt <- table.createdAt()
-    yield User(id, name, age, updatedAt, createdAt)
+    yield User(id, name, age, status, updatedAt, createdAt)
 
   override def run(args: List[String]): IO[ExitCode] =
     (for
       //updated <- sql"INSERT INTO user (id, name, age) VALUES ($None, 'test', $None)".update()
-      user <- (sql"SELECT * FROM user" ++ sql"WHERE id = ${ 1 }").query
+      user <- sql"SELECT * FROM user WHERE id = ${ 1 }".query
     yield
       //println(updated)
       println(user)
